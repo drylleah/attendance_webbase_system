68b6f9bb7f636f735ccb56ceb2aebdbc97561d8a
Elipott <jemimahelisha.zamoranos@lorma.edu>
2026-07-06 10:01:22 +0800

    auth.js

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -39,7 +39,7 @@ router.post('/login', async (req, res) => {
     });
   } catch (err) {
     console.error('Login error:', err);
-    res.status(500).json({ error: 'Server error. Subukan ulit.' });
+    res.status(500).json({ error: '' });
   }
 });
 
